21efa78f9a8bc196b81604c5814fb77f85b3eb3c
Najak <najak2007@gmail.com>
2026-07-18 02:09:36 +0900

    Init

---
+++ StudentFullCare/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  StudentFullCare
+//
+//  Created by najak on 7/18/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ StudentFullCare/StudentFullCareApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudentFullCareApp.swift
+//  StudentFullCare
+//
+//  Created by najak on 7/18/26.
+//
+
+import SwiftUI
+
+@main
+struct StudentFullCareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
